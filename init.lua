52336cfad009627570e20ad214c5a493328b6205
Minh Vu <minhd_vu@yahoo.com>
2022-10-13 22:46:18 -0400

    add nv chad status line

--- init.lua
+++ init.lua
@@ -241,6 +241,27 @@ local config = {
     },
   },
 
+  -- add new user interface icon
+  icons = {
+    VimIcon = "",
+  },
+
+  -- modify variables used by heirline but not defined in the setup call directly
+  heirline = {
+    -- define the separators between each section
+    separators = {
+      left = { "", " " }, -- separator for the left side of the statusline
+      right = { " ", "" }, -- separator for the right side of the statusline
+    },
+    -- add new colors that can be used by heirline
+    colors = {
+      blank_bg = "#5c6370",
+      file_info_bg = "#3e4452",
+      nav_icon_bg = "#89b06d",
+      folder_icon_bg = "#ec5f67",
+    },
+  },
+
   -- Configure plugins
   plugins = {
     init = {
