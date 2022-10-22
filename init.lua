162b1aa35a72bb60c85e61bc3a04bbbfd570206c
Minh Vu <minhd_vu@yahoo.com>
2022-10-22 14:35:08 -0400

    add contrast to material theme

--- init.lua
+++ init.lua
@@ -260,13 +260,21 @@ local config = {
         "marko-cerovac/material.nvim",
         config = function()
           require("material").setup {
+            contrast = {
+              terminal = true, -- Enable contrast for the built-in terminal
+              sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+              floating_windows = false, -- Enable contrast for floating windows
+              cursor_line = false, -- Enable darker background for the cursor line
+              non_current_windows = true, -- Enable darker background for non-current windows
+              filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+            },
             plugins = { -- Uncomment the plugins that you use to highlight them
               -- Available plugins:
               -- "dap",
               "dashboard",
               "gitsigns",
               -- "hop",
-              -- "indent-blankline",
+              "indent-blankline",
               -- "lspsaga",
               -- "mini",
               -- "neogit",
