d7921b12ffdb71d6b99bbcb6b5a3010936a3678b
minhd-vu <minhd_vu@yahoo.com>
2022-10-31 23:50:22 -0400

    use project manual mode

--- init.lua
+++ init.lua
@@ -334,6 +334,8 @@ local config = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
+            manual_mode = true,
+            silent_chdir = false,
           }
           require("telescope").load_extension "projects"
         end,
